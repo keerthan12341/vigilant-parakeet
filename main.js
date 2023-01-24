8e795240e377f5f4ad2979ea261c3d46fe65e5ad
keerthan12341 <106748066+keerthan12341@users.noreply.github.com>
2023-01-24 17:44:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+function preload() {
+clown_nose = loadImage('https://i.postimg.cc/5tNkr7Fn/m.png')
+}
+function setup() {
+    canvas = createCanvas(300,300)
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet is initialized like a1 😱')
+}
+
+function gotPoses(results)
+    {
+        if(results.length > 0)
+        {
+            console.log(results);
+            noseX = results[0].pose.nose.x -25;
+        noseY = results[0].pose.nose.y ;
+            console.log("nose x = " + results[0].pose.nose.x)
+            console.log("nose y = " + results[0].pose.nose.y)
+
+        }
+    }
+function draw(){
+ image(video, 0, 0, 300, 300);
+ image(clown_nose,noseX,noseY,50,50)
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
